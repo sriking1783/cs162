59d3771dd120d0ca5f2d51e99c81cfbce2f98105
Srikanth Astaputhra <sriking007@gmail.com>
2015-08-31 13:09:53 +0000

    implemented out redirection

--- hw1/shell.c
+++ hw1/shell.c
@@ -7,7 +7,9 @@
 #include <termios.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include <dirent.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
 #define FALSE 0
 #define TRUE 1
 #define INPUT_STRING_SIZE 80
@@ -24,7 +26,7 @@ int cmd_quit(tok_t arg[]) {
 }
 
 int cmd_help(tok_t arg[]);
-
+int cmd_change_directory(tok_t arg[]);
 
 /* Command Lookup table */
 typedef int cmd_fun_t (tok_t args[]); /* cmd functions take token array and return int */
@@ -37,7 +39,50 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_quit, "quit", "quit the command shell"},
+  {cmd_change_directory, "cd", "change the directory"},
 };
+
+char *get_path_from_file(char *file)
+{
+  char* token;
+  char *path = getenv("PATH");
+  struct dirent *ent;
+  while ((token = strsep(&path, ":")) != NULL)
+  {
+    DIR *dir;
+    if ((dir = opendir (token)) != NULL) {
+      while ((ent = readdir (dir)) != NULL)
+      {
+        int rc = strcmp(ent->d_name, file);
+        if(rc == 0)
+        {
+          return token;
+          break;
+        }
+      }
+      closedir(dir);
+    }
+  }
+  return NULL;
+}
+
+char * current_directory()
+{
+  char cwd[1024];
+  if(getcwd(cwd, sizeof(cwd)) != NULL)
+    return cwd;
+  else
+    return "Current directory not found!";
+}
+
+int cmd_change_directory(tok_t arg[]) {
+  int ret;
+  ret = chdir(*arg);
+   if(ret!=0){
+     perror("Error: ");
+   }
+  return 0;
+}
 
 int cmd_help(tok_t arg[]) {
   int i;
@@ -98,10 +143,74 @@ void add_process(process* p)
 process* create_process(char* inputString)
 {
   /** YOUR CODE HERE */
-  return NULL;
 }
 
+void exec_process(char* inputString)
+{
 
+  char* token;
+  int i = 0;
+  char *tofree[4];
+  char path_file[1000] = "" ;
+  int pipe_to_child[2];
+  int pipe_from_child[2];
+  char *command ;
+  char buf[256];
+  char* buf_pointer = malloc(sizeof(char)*(257));
+  char* file = NULL;
+  while ((token = strsep(&inputString, "-")) != NULL)
+  {
+    tofree[i]=malloc(20);
+    tofree[i] = token;
+    i = i + 1;
+  }
+  if(i>2){
+    file = tofree[3];
+  }
+  pipe(pipe_to_child);
+  pipe(pipe_from_child);
+  int pID = fork();
+  if (pID == 0){
+    char *path = get_path_from_file(tofree[0]);
+    strcat(path, "/");
+    strcat(path, tofree[0]);
+    strcat(path,path_file);
+    command = tofree[0];
+    close(pipe_to_child[1]);
+    dup2(pipe_to_child[0], fileno(stdin));
+    dup2(pipe_from_child[1], fileno(stdout));
+    execl(path, command, tofree[1] , NULL);
+    *path = NULL;
+    
+    exit(0);
+  }
+  else if(pID < 0){
+    perror("Error1: ");
+  }
+  else {
+    int returnStatus;
+
+    close(pipe_to_child[0]);
+    waitpid(pID, &returnStatus, 0);
+    read(pipe_from_child[0], buf, sizeof(buf));
+    *buf_pointer = buf;
+    if(i>2){  
+      FILE *fp;
+      fp = fopen(file, "w");
+      if (fp == NULL)
+      {
+        perror("Error: ") ;
+        exit(1);
+      }
+      fprintf(fp, "%s", buf);
+      fclose(fp);
+    }
+    else{
+      printf("%s", buf);
+    }
+    free(buf_pointer);
+  }
+}
 
 int shell (int argc, char *argv[]) {
   char *s = malloc(INPUT_STRING_SIZE+1);			/* user input string */
@@ -117,15 +226,43 @@ int shell (int argc, char *argv[]) {
   printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
 
   lineNum=0;
-  fprintf(stdout, "%d: ", lineNum);
+  char cwd[1024];
+  char* inputString;
+  //int in, out;
+  fprintf(stdout, "%s: ", current_directory());
   while ((s = freadln(stdin))){
     t = getToks(s); /* break the line into tokens */
     fundex = lookup(t[0]); /* Is first token a shell literal */
     if(fundex >= 0) cmd_table[fundex].fun(&t[1]);
     else {
-      fprintf(stdout, "This shell only supports built-ins. Replace this to run programs as commands.\n");
+      int length_t = sizeof(t)/sizeof(t[0]);
+   
+      if(t[2] != NULL && strcmp(t[2], ">") == 0)
+      {
+        inputString = malloc(strlen(t[0]) + strlen(t[1]) + strlen(t[2]) + strlen(t[3]) + 1); 
+        strcpy(inputString, t[0]);
+        strcat(inputString, "-");
+        strcat(inputString, t[1]);
+        strcat(inputString, "-");
+        strcat(inputString, t[2]);
+        strcat(inputString, "-");
+        strcat(inputString, t[3]);
+      } 
+      else
+      {
+        inputString = malloc(strlen(t[0]) + strlen(t[1]) + 1); 
+        strcpy(inputString, t[0]);
+        strcat(inputString, "-");
+        strcat(inputString, t[1]);
+      }
+      //printf("%s\n", inputString);
+      //dup2(out, 1);
+      //close(out);
+      exec_process(inputString);
+      //printf("%s %s %s %d %d %d\n", t[0], t[1], t[2], sizeof(t), sizeof(int), t[2] == ">");
+      // fprintf(stdout, "This shell only supports built-ins. Replace this to run programs as commands.\n");
     }
-    fprintf(stdout, "%d: ", lineNum);
+    fprintf(stdout, "%s: ", current_directory());
   }
   return 0;
 }
